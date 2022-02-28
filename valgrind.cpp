26eab2d255e6f728a4919cce34cd45e8c28d2436
Pietro Ferrara <pietro.ferrara@unive.it>
2022-02-28 10:00:22 +0100

    lecture 7: memory leak

--- valgrind.cpp
+++ valgrind.cpp
@@ -5,10 +5,5 @@ using namespace std;
 int main()
 {
     int *a = new int[10];
-    int i;
-    for (int j = 0; j < 10; j++)
-        a[j] = j;
-    cin >> i;
-    cout << a[i] << endl;
-    delete[] a;
+    a[0] = 1;
 }
