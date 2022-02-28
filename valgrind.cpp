f160b075b708e430624d405f780d74281592c885
Pietro Ferrara <pietro.ferrara@unive.it>
2022-02-28 09:51:28 +0100

    Lecture 7: out of bounds

--- valgrind.cpp
+++ valgrind.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main()
 {
     int *a = new int[10];
-    for (int i = 0; i < 10; i++)
-        a[i] = 1;
+    cout << a[-1] << endl;
+    cout << a[10] << endl;
     delete[] a;
-    cout << a[5] << endl;
 }
