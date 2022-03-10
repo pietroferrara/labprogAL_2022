8164949910f18c11a4e507fcf6add129144a6efe
Pietro Ferrara <pietro.ferrara@unive.it>
2022-03-10 09:58:25 +0100

    calculator without proper cin extraction

--- lecture9_calculator.cpp
+++ lecture9_calculator.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+struct expression_not_supported
+{
+};
 
 bool isNotADigit(char a)
 {
@@ -9,6 +14,21 @@ bool isNotADigit(char a)
 
 int extractNumber(string str)
 {
+    int result = 0;
+    /*Implementation left to right
+    for (int i = 0; i < str.size(); i++)
+    {
+        result = result * 10 + (str.at(i) - '0');
+    }*/
+    /*Implementation right to left
+    for (int i = 0; i < str.size(); i++)
+    {
+        int index = str.size() - i - 1;
+        result = result + (str.at(index) - '0') * std::pow(10, i);
+    }
+    */
+    result = stoi(str);
+    return result;
 }
 
 int indexAfterNumber(string str)
@@ -21,11 +41,53 @@ int indexAfterNumber(string str)
     return str.length();
 }
 
+int performOperation(char op, int left, int right)
+{
+    switch (op)
+    {
+    case '+':
+        return left + right;
+    case '-':
+        return left - right;
+    case '*':
+        return left * right;
+    case '/':
+        return left / right;
+    default:
+        throw expression_not_supported();
+    }
+}
+
+int compute_result_of_expression(string input)
+{
+    string left = input.substr(0, indexAfterNumber(input));
+    char op = input.at(indexAfterNumber(input));
+    string right = input.substr(
+        indexAfterNumber(input) + 1,
+        input.size() - (indexAfterNumber(input) + 1));
+    int leftNumber = extractNumber(left),
+        rightNumber = extractNumber(right);
+    return performOperation(op, leftNumber, rightNumber);
+}
+
 int main()
 {
     string input;
-    cout << "Please type an expression:" << endl;
-    cin >> input;
-    cout << "First index not a digit:" << indexAfterNumber(input);
-    cout << "The operator is:" << input.at(indexAfterNumber(input));
+    bool correct_expression = false;
+    int result;
+    while (!correct_expression)
+    {
+        cout << "Please type an expression:" << endl;
+        cin >> input;
+        try
+        {
+            result = compute_result_of_expression(input);
+            correct_expression = true;
+        }
+        catch (expression_not_supported)
+        {
+            cout << "The expression you typed in is not correct, please try again" << endl;
+        }
+    }
+    cout << "The result is " << result << endl;
 }
