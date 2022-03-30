0b7a4be704914c93327e7c16dd9fe6c5bb5eaba8
Pietro Ferrara <pietro.ferrara@unive.it>
2022-03-30 10:18:03 +0200

    lecture 15: string stream

--- lecture14_csvreader.cpp
+++ lecture14_csvreader.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
 int main()
 {
-    int value;
-    cin >> value;
-    cout << value << endl;
-    if (cin.good())
-        cout << "cin is good" << endl;
-    if (cin.eof())
-        cout << "cin is at the end" << endl;
-    if (cin.fail())
-        cout << "cin failed" << endl;
-    if (cin.bad())
-        cout << "cin is broken" << endl;
-    cout << "End of everything" << endl;
+    string date{"11/12/2020 today"};
+    istringstream iss{date};
+    /*string day, month, year;
+    getline(iss, day, '/');
+    getline(iss, month, '/');
+    getline(iss, year, '/');
+
+    int dd = stoi(day);
+    int mm = stoi(month);
+    int yy = stoi(year);*/
+
+    int dd, mm, yy;
+    char sep1, sep2;
+    iss >> dd >> sep1 >> mm >> sep2 >> yy;
+    if (iss.fail() || !iss.eof())
+        cout << "Error";
+    else
+        cout << "day " << to_string(dd) << " month " << to_string(mm) << " year " << to_string(yy);
 }
